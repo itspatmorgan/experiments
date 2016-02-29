64c466869eaf4e90de3a7db7c7f544f4649576d3
patrickmmorgan <pamorgan12@gmail.com>
2016-02-29 14:52:32 -0500

    added functions for reversing a string

--- helper-functions.js
+++ helper-functions.js
@@ -1,17 +1,45 @@
-var utility = {
-  _addEventListenerByClass: function(className, event, func){
+var PM = PM || {};
+
+PM.Utility = (function() {
+
+  function addEventListenerByClass(className, event, func) {
     var classlist = document.getElementsByClassName(className);
 
     for (var i = 0, len = classlist.length; i < len; i ++) {
       classlist[i].addEventListener(event, func, false);
     }
-  },
+  }
 
-  _removeClass: function(className, selector) {
-    var x = document.querySelectorAll('.' + selector);
+  function removeClass(className, selector) {
+    var elementsWithClass = document.querySelectorAll('.' + selector);
 
-    [].forEach.call(x, function(n){
+    [].forEach.call(elementsWithClass, function(n){
       n.classList.remove(className);
     });
   }
-};
+
+  function reverseStringWithMethods(str) {
+    var reversed = str.split("").reverse().join("");
+
+    return reversed;
+  }
+
+  function reverseStringWithLoop(str) {
+    var reversed_string = "";
+
+    for (var i = str.length - 1; i >= 0; i--) {
+      reversed_string = reverseString + str.charAt(i);
+    }
+
+    return  reversed_string;
+  }
+
+  return {
+    addEventListenerByClass: addEventListenerByClass,
+    removeClass: removeClass,
+    reverseStringWithMethods: reverseStringWithMethods,
+    reverseStringWithLoop: reverseStringWithLoop
+  };
+});
+
+
